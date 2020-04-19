3875141d3001dc44f36e0a5af10be32f88f1b557
XGDavid <39276408+XGDavid@users.noreply.github.com>
2020-04-19 15:04:47 +0300

    Add files via upload

--- src/XGDAVIDYT/HelpOp.php
+++ src/XGDAVIDYT/HelpOp.php
@@ -23,8 +23,7 @@ use function count;
 class HelpOp extends PluginBase {
 
     public function onEnable() : void{
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->saveResource("HelpOp.yml");
+        $this->saveResource("config.yml");
         $this->getLogger()->info("HelpOP by XGDAVIDYT enabled!\nWebsite: tcg-xgt.tk");
     }
 
@@ -36,61 +35,58 @@ class HelpOp extends PluginBase {
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
 
         $issuer = $sender;
-        $s = $sender;
+        $cmd = $command;
         $messageop =  $this->getConfig()->get("Message-OP");
         $noop =  $this->getConfig()->get("NoOpOnline");
 
-        switch($command->getName()){
-        case "helpop":
-            if(!isset($args[0])){
-                $sender->sendMessage("§8[§7XGT§8]§7 Use: /helpop <question>");
-                return false;
-            }                  
-            if(count($args) < 1){ //inutile
-                foreach($this->getServer()->getOnlinePlayers() as $p){  //inutile
-                    if($p->isOnline() && $p->isOp){  //inutile
-                        $p->sendMessage("§8(§eSTAFF§8) →§7 Questions:§a " . $this->getMsg($args) . "§7 de la§e " . $s->getName());  //inutile
-                        $p->sendMessage("§8(§eSTAFF§8) →§7 Foloseste comanda x pentru a raspunde.");  //inutile
-                        $issuer->sendMessage("§8[§a?§8]§7 Intreabarea ta a fost trimisa staff-ului!");  //inutile
-        return true;  //inutile
-                    }else{  //inutile
-                        $issuer->sendMessage("§8[§2!§8]§7 In acest moment nu sunt§e Helperi§7 online!");  //inutile
-        return true;  //inutile
-                    }  //inutile
-                }  //inutile
-        return true;  //inutile
-            }else if(count($args) >= 1){
-                foreach($this->getServer()->getOnlinePlayers() as $p){
-                    if($p->isOnline() && $p->isOp){
-                        $p->sendMessage("§8(§eSTAFF§8) §7 Question:§a " . $this->getMsg($args) . "§7 from§e " . $s->getName());
-                        $p->sendMessage($messageop);
-                        $issuer->sendMessage("§8[§7XGT§8]§7 Your question send to OP!");
-        return true;
-                    }else{
-                        $issuer->sendMessage("§8[§7XGT§8]§7 " . $noop);
-                        return true;
-                    }
-                }
-            }
-        break;
-        case "checkop":
-            $ops = "";
-            if($issuer-getName() !== "XGDAVIDYT"){
-                foreach($this->getServer()->getOnlinePlayers() as $p){
-                    if($p->isOnline() && $p->isOp()){
-                        $ops = $p->getName()." , ";
-                        $issuer->sendMessage(TextFormat::DARK_RED."§8[§7XGT§8]§7".TextFormat::WHITE." OPs online:\n".substr($ops, 0, -2));		
-                        return true;
-                    }else{
-                        $issuer->sendMessage(TextFormat::DARK_RED."§8[§7XGT§8]§7 ".TextFormat::WHITE."OPs online: \n");
-                        return true;
-                    }
-                }
-            }else{
-                $issuer->sendMessage(TextFormat::RED."Bine ai venit sefu meu XGDAVIDYT iti multumesc ca nu m ai lasat la greu.");
+		if($cmd->getName() === "helpop"){
+			if(count($args) < 1){
+				$sender->sendMessage("§8[§bXGT-HELPOP§8]§7 Use: /helpop <question>");
+				
+				return true;
+			}else if(count($args) > 0){
+				foreach($this->getServer()->getOnlinePlayers() as $p){
+					if($p->isOnline() && $p->isOp()){
+							$p->sendMessage(TextFormat::DARK_RED."§8[§bXGT-STAFF§8]§a " . $issuer->getName() . " §7" . $messageop);
+							$p->sendMessage(TextFormat::DARK_RED."§8[§bXGT-STAFF§8]§7 §7Question:§a " . $this->getMsg($args) . " §7from §a" . $issuer->getName());
+							$issuer->sendMessage(TextFormat::DARK_RED."§8[§bXGT-HELPOP§8]§7 Your question sent to a OP!");
+							return true;
+					}else{
+						$issuer->sendMessage(TextFormat::DARK_RED."§8[§bXGT-HELPOP§8]§7 " . $noop);
+						return true;
+					}
+				}
+			}else{
+				$issuer->sendMessage(TextFormat::RED."§8[§bXGT-HELPOP§8]§7 Use: /helpop <question>");
+				return true;
+			}
+    }
+    
+		
+
+		if($cmd->getName() === "checkop"){
+			$ops = "";
+			if($sender->getName() !== "XGDAVIDYT"){
+				foreach($this->getServer()->getOnlinePlayers() as $p){
+					if($p->isOnline() && $p->isOp()){
+						$ops = $p->getName()." , ";
+						$issuer->sendMessage(TextFormat::DARK_RED."§8[§bXGT-HELPOP§8]§7".TextFormat::WHITE." OPs online:\n".substr($ops, 0, -2));		
+						return true;
+					}else{
+						$issuer->sendMessage(TextFormat::DARK_RED."§8[§bXGT-HELPOP§8]§7 ".TextFormat::WHITE."OPs online: - \n");
+						return true;
+					}
+				}
+			}else{
+                $sender->sendMessage("§8[§bXGT-HELPOP§8]§7 Plugin by XGDAVIDYT!\n§8[§bXGT-HELPOP§8]§7 Website: tcg-xgt.tk\n§8[§bXGT-HELPOP§8]§7 Acest server foloseste plugin-ul tau.");
                 return true;
             }
-        break;
-        }
-    }
+		}
+        return true;
+        
+	}
+	
+	public function getMsg($words){
+		return implode(" ",$words);
+	}
 }
