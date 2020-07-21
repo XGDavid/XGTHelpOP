ea1ad5f9531139325a78db45b21297f3e1cb68ad
XGDavid <39276408+XGDavid@users.noreply.github.com>
2020-07-22 00:06:00 +0300

    :D

--- src/XGDAVIDYT/XGTHelpOP/Main.php
+++ src/XGDAVIDYT/XGTHelpOP/Main.php
@@ -17,49 +17,77 @@ use pocketmine\utils\Utils;
 use pocketmine\plugin\Plugin;
 use pocketmine\plugin\PluginManager;
 
-class Main extends PluginBase{
+class Main extends PluginBase implements Listener{
 
 	public function onEnable() : void{
-        $this->saveResource("config.yml");
+		$this->saveResource("config.yml");
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		if($this->getConfig()->get("version") !== 1.0){
+			$this->getServer()->getLogger()->error("[XGTHelpOP] Config is outdata!");
+			$this->getServer()->getPluginManager()->disablePlugin($this);
+		}
     }
 
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
 
         $messageop =  $this->getConfig()->get("Message-OP");
-        $noop =  $this->getConfig()->get("NoOpOnline");
+		$noop =  $this->getConfig()->get("NoOpOnline");
+		$noacces =  $this->getConfig()->get("NoAcces");
 
 		if($command->getName() === "helpop"){
-			if(count($args) < 0){
-				$sender->sendMessage("§8[§bXGT-HELPOP§8]§7 Use: /helpop <question>");
-				
-				return true;
-			}elseif(count($args) > 0){
-				foreach($this->getServer()->getOnlinePlayers() as $p){
-					if($p->isOp()){
-						$p->sendMessage(TextFormat::DARK_RED."§8[§bXGT-STAFF§8]§a " . $sender->getName() . " §7" . $messageop);
-						$p->sendMessage(TextFormat::DARK_RED."§8[§bXGT-STAFF§8]§7 §7Question:§a " . $this->getMsg($args) . " §7from §a" . $sender->getName());
-						$sender->sendMessage(TextFormat::DARK_RED."§8[§bXGT-HELPOP§8]§7 Your question sent to a OP!");
-						return true;
-					}else{
-						$sender->sendMessage(TextFormat::DARK_RED."§8[§bXGT-HELPOP§8]§7 " . $noop);
-						return true;
+			if(!$sender instanceof Player){
+				$sender->sendMessage("§8[§bXGT-HELPOP§8]§c Use in-game!");
+			}else{
+				if(!$sender->hasPermission("xgthelpop.helpop")){
+					$sender->sendMessage("§8[§bXGT-HELPOP§8]§c " . $noacces);
+					return;
+				}
+				if(count($args) < 0){
+					$sender->sendMessage("§8[§bXGT-HELPOP§8]§7 Use: §b/helpop §7<§bquestion§7>");
+					return true;
+				}elseif(count($args) > 0){
+					foreach($this->getServer()->getOnlinePlayers() as $p){
+						if($p->isOp()){
+							$name = $sender->getName();
+							$player = $sender->getServer()->getPlayerExact($name);
+							if(!isset($this->cdhelpop[$player->getName()])){
+								$this->cdhelpop[$player->getName()] = time() + 300;
+								$p->sendMessage("§8[§bXGT-STAFF§8]§a " . $name . " §7" . $messageop);
+								$p->sendMessage("§8[§bXGT-STAFF§8]§7 §7Question:§a " . $this->getMsg($args) . " §7from §a" . $name);
+								$sender->sendMessage("§8[§bXGT-HELPOP§8]§7 Your question sent to a OP!");
+							}else{
+								if(time() <  $this->cdhelpop[$player->getName()]){
+									$ramas =  $this->cdhelpop[$player->getName()] - time();
+									$player->sendMessage("§l§8[§2!§8] »§r §7You can use this comand again in§6 ".$ramas." §7seconds!");
+								}else{
+									unset($this->cdhelpop[$player->getName()]);
+									$sender->sendMessage("§8[§bXGT-HELPOP§8]§7 Use: §b/helpop §7<§bquestion§7>");
+								}
+							}
+						}else{
+							$sender->sendMessage("§8[§bXGT-HELPOP§8]§7 " . $noop);
+						}
 					}
 				}
 			}
 		}
-    
-		
-
+		return true;
 		if($command->getName() === "checkop"){
-			$ops = "";
-			foreach($this->getServer()->getOnlinePlayers() as $p){
-				if($p->isOp()){
-					$ops = $p->getName()." , ";
-					$sender->sendMessage(TextFormat::DARK_RED."§8[§bXGT-HELPOP§8]§7".TextFormat::WHITE." OPs online:\n".substr($ops, 0, -2));		
-					return true;
-				}else{
-					$sender->sendMessage(TextFormat::DARK_RED."§8[§bXGT-HELPOP§8]§7 ".TextFormat::WHITE."OPs online: - \n");
-					return true;
+			if(!$sender instanceof Player){
+				$sender->sendMessage("§8[§bXGT-HELPOP§8]§c Use in-game!");
+			}else{
+				if(!$sender->hasPermission("xgthelpop.checkop")){
+					$sender->sendMessage("§8[§bXGT-HELPOP§8]§c " . $noacces);
+					return;
+				}
+				$ops = "";
+				foreach($this->getServer()->getOnlinePlayers() as $p){
+					if($p->isOp()){
+						$ops = $p->getName()." , ";
+						$sender->sendMessage("§8[§bXGT-HELPOP§8]§7 OPs online:\n".substr($ops, 0, -2));	
+					}else{
+						$sender->sendMessage("§8[§bXGT-HELPOP§8]§7 OPs online: " . $noop);
+					}
 				}
 			}
 		}
